b9f77ddbe420313f4a5936aac610c76d51932050
nhiendie57-creator <nhiendie57@gmail.com>
2026-08-14 07:40:24 +0700

    Import index.css in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./index.css"; 
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./config/firebase";
 import { UserProfile, TabType } from "./types";
